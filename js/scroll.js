8af327dd432616ddafea588286c3bb9cf59ae282
Alex De la Cruz <alexxruff@yahoo.com>
2017-12-25 22:42:53 -0600

    Se agregó responsibidad a la página

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
-
+	console.log("Mi resolución de pantalla es: "+screen.width + " px por "+screen.height)
+	$('#muestraMenuA').click(function(){
+		$('#menuCompleto').removeClass('menuOculto')
+		$('#ocultaMenuA').removeClass('none')
+		$('#menuCompleto').addClass('menuMostrado')
+		$('#muestraMenuA').addClass('none')
+	});
+	$('#ocultaMenuA').click(function(){
+		$('#menuCompleto').removeClass('menuMostrado')
+		$('#muestraMenuA').removeClass('none')
+		$('#menuCompleto').addClass('menuOculto')
+		$('#ocultaMenuA').addClass('none')
+	});
+	$('#serviciosResponsive').click(function(){
+		$('#menuCompleto').removeClass('menuMostrado')
+		$('#muestraMenuA').removeClass('none')
+		$('#menuCompleto').addClass('menuOculto')
+		$('#ocultaMenuA').addClass('none')
+	});
 	$('#service').click(function(){
 		$('body, html').animate({
 			scrollTop: $('#servicios').offset().top
@@ -15,7 +33,7 @@ $(document).ready(function(){
 
 	$(window).scroll(function(){
 		var barra = $(window).scrollTop();
-		if (barra > 60) {
+		if (barra > 60 && screen.width > 640) {
 			$('.menuScroll').css({
 				'display': 'block',
 				'transition': '1s'
@@ -27,7 +45,7 @@ $(document).ready(function(){
 			});		
 		}
 
-		if (barra > 237) {
+		if (barra > 237 || screen.width < 640) {
 			$('#antesDeContratar').removeClass('none')
 			$('#antesDeContratar').addClass('fadeInUp animated')
 		}else{
@@ -35,21 +53,21 @@ $(document).ready(function(){
 
 		}
 
-		if (barra > 590) {
+		if (barra > 590 || screen.width < 640) {
 			$('#siniestros').removeClass('none')
 			$('#siniestros').addClass('fadeInDown animated')
 		}else{
 			$('#siniestros').addClass('none')
 
 		}
-		if (barra > 895) {
+		if (barra > 895 || screen.width < 640) {
 			$('#vigencia').removeClass('none')
 			$('#vigencia').addClass('fadeInUp animated')
 		}else{
 			$('#vigencia').addClass('none')
 
 		}
-		if (barra > 1100) {
+		if (barra > 1100 || screen.width < 640) {
 			$('#renovacion').removeClass('none')
 			$('#renovacion').addClass('fadeInDown animated')
 		}else{
